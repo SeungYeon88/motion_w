bfcec8b679ee2125b76d4f9df544bd4f553fbb8d
SeungYeon88 <tmddus88@gmail.com>
2024-07-11 16:53:10 +0900

    motion w login writing

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 import selenium
+import time
+
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver import ActionChains
+
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.ui import WebDriverWait
+
 
 op = Options()
 op.add_experimental_option("detach", True)
@@ -11,3 +22,10 @@ driver = webdriver.Chrome(options=op)
 driver.get(url=motion_w_url)
 
 # driver.find_element()
+
+class login:
+    
+    def success_login():
+        window_login = driver.find_element((By.XPATH,'<input type="text" name="mainMemID" placeholder="운영자 아이디" value="" required="">'))
+        ActionChains(driver).send_keys_to_element(window_login,"triuptheme3").perform()
+        time.sleep(0.5)
