940c1b02d4869ea674f5af49bf308598c7e6a2c2
know77701 <know77701@gmail.com>
2024-07-22 18:03:59 +0900

    product category function update

--- function/product.py
+++ function/product.py
@@ -1,29 +1,38 @@
+import selenium
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import NoAlertPresentException
+from selenium.common.exceptions import NoAlertPresentException, StaleElementReferenceException
 import time
 
-# rsrv_url = 'https://manager.motionecosystem.com/'
-
-# service = Service()
-# chrome_options = Options()
-# chrome_options.add_argument('--disable-blink-features=AutomationControlled')
-# chrome_options.add_experimental_option("debuggerAddress", "127.0.0.1:9222")
-
-
-# driver = webdriver.Chrome(service=service, options=chrome_options)
-# driver.get(url=rsrv_url)
-# driver.maximize_window()
-# driver.refresh()
 
 class Product():
     def product_starter(driver):
+        # Product.success_login(driver)
         Product.product_category_remove(driver)
+
+    def get_table_list(id_value, tag_name):
+        category_list = WebDriverWait(driver, timeout=3).until(EC.presence_of_all_elements_located((By.ID, id_value)))
+        td_elements = WebDriverWait(category_list[0], timeout=3).until(EC.presence_of_all_elements_located((By.TAG_NAME, tag_name)))
+        return td_elements
         
+    
+    def success_login(driver):
+        window_login = driver.find_element(By.XPATH,'//*[@id="frmLogin"]/input')
+        window_login = driver.find_element(By.NAME,'mainMemID')
+        window_login.send_keys('triuptheme3')
+        time.sleep(0.5)
+        
+        window_login = driver.find_element(By.NAME,'mainMemPwd')
+        window_login.send_keys('qwer1234!@#')
+        
+        login_btn = driver.find_element(By.XPATH,'//*[@id="frmLogin"]/div[1]/button')
+        login_btn.click()
+        
+    
     def scroll_into_view(driver, element):
         driver.execute_script("arguments[0].scrollIntoView(true);", element)
         time.sleep(0.5)
@@ -36,17 +45,20 @@ class Product():
         except NoAlertPresentException:
             return False
 
-    def find_category(save_value,driver):
-        category_list = WebDriverWait(driver, timeout=3).until(EC.presence_of_all_elements_located((By.ID, 'dragSort')))
-        td_elements = WebDriverWait(category_list[0], timeout=3).until(EC.presence_of_all_elements_located((By.TAG_NAME, 'td')))
-        result = False
-
-        for index in td_elements:
-            print(index.text)
-            if index == save_value:
-                result = True
-                return result
-        return result
+    def find_category(save_value):
+        trise = 0
+        while trise <= 3:
+            try:
+                td_elements = Product.get_table_list('dragSort', 'td')
+                
+                for element in td_elements:
+                        if element.text == save_value:
+                            return True
+            except StaleElementReferenceException:
+                trise += 1
+                continue
+            break
+        return False
 
     def product_category_save(driver):
         save_value = "카테고리 등록 테스트"
@@ -79,8 +91,7 @@ class Product():
     def product_category_remove(driver):
         category_title = Product.product_category_save(driver)
         if category_title != None:
-            category_list = WebDriverWait(driver, timeout=3).until(EC.presence_of_all_elements_located((By.ID, 'dragSort')))
-            tr_list = WebDriverWait(category_list[0], timeout=3).until(EC.presence_of_all_elements_located((By.TAG_NAME, 'tr')))
+            tr_list = Product.get_table_list('dragSort', 'tr')
 
             save_category = None
             
@@ -90,7 +101,7 @@ class Product():
             
             if save_category:
                 delete_button = WebDriverWait(save_category, 10).until(EC.presence_of_element_located((By.XPATH, ".//button[contains(text(), '삭제')]")))
-                delete_button.click()        
+                delete_button.click()
                 WebDriverWait(driver, 10).until(EC.alert_is_present())
                 driver.switch_to.alert.accept()
                 result = Product.find_category(category_title,driver)
@@ -100,6 +111,34 @@ class Product():
                     print("삭제 성공")
         else:
             print("카테고리 등록실패")
+            
+            
+    def product_category_update(driver):
+        category_title = Product.product_category_save(driver)
+        if category_title != None:
+            save_category = None
+            tr_list = Product.get_table_list('dragSort', 'tr')
+            
+            for tr in tr_list:
+                if category_title in tr.text:
+                    save_category = tr
+            if save_category:
+                update_btn = WebDriverWait(save_category, 10).until(EC.presence_of_element_located((By.XPATH, ".//button[contains(text(), '수정')]")))
+                update_btn.click()
+                update_edit = WebDriverWait(save_category, 10).until(EC.presence_of_element_located((By.XPATH, "//*[@id='name239']")))
+                update_edit.send_keys("카테고리 수정 테스트")
+                
+            
+rsrv_url = 'https://manager.motionecosystem.com/'
 
+service = Service()
+chrome_options = Options()
+chrome_options.add_argument('--disable-blink-features=AutomationControlled')
+chrome_options.add_experimental_option("debuggerAddress", "127.0.0.1:9222")
 
+driver = webdriver.Chrome(service=service, options=chrome_options)
+driver.get(url=rsrv_url)
+driver.maximize_window()
+driver.refresh()
 
+Product.product_category_update(driver)
